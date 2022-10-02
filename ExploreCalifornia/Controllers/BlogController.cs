e95a7bfc85360bb472d6ed5fee81c488fe9022d3
Ben Mitchell <ben.mitchell@instantonline.nz>
2022-10-02 16:21:57 +1300

    Make post create page

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ExploreCalifornia.Controllers
 {
@@ -15,8 +17,41 @@ namespace ExploreCalifornia.Controllers
         [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Jess Chadwick",
+                Body = "lorem ipsum dolor...",
+            };
+
+            //using the viewbag.
+            //ViewBag.Title = "My blog post";
+            //ViewBag.Posted = DateTime.Now;
+            //ViewBag.Author = "Jess Chadwick";
+            //ViewBag.Body = "lorem ipsum dolor...";
             return View();
             //return new ContentResult { Content = year.ToString() };
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+
+            return View();
         }
     }
 }
---
+++ ExploreCalifornia/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ExploreCalifornia.Models
+{
+	public class Post
+	{
+		[Display(Name = "Post Title")]
+		[Required]
+		[DataType(DataType.Text)]
+		[StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters long")]
+		public string Title { get; set; }
+		public string Author { get; set; }
+        [Required]
+		[DataType(DataType.MultilineText)]
+        [StringLength(100, ErrorMessage = "Title must be at least 100 characters long")]
+        public string Body { get; set; }
+		public DateTime Posted { get; set; }
+	}
+}
